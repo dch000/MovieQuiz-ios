b24887ee47bf493296e6104e600fc682f711466b
dch000 <dmitriy.ev.ch@yandex.ru>
2023-01-29 22:34:19 +0300

    по итогу дня

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,15 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Чеботарев on 29.01.2023.
+//
+
+import Foundation
+
+// Реализация логики приложения
+private struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Чеботарев on 29.01.2023.
+//
+
+import Foundation
+
+// для состояния "Результат квиза"
+private struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
