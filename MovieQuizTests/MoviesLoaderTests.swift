8d89ed0f3283aff6725323961e542d62fa856041
dch000 <dmitriy.ev.ch@yandex.ru>
2023-02-28 20:45:57 +0300

    UT

--- MovieQuizTests/MoviesLoaderTests.swift
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -29,10 +29,23 @@ class MoviesLoaderTests: XCTestCase {
     
     func testFailureLoading() throws {
         //Given
+        let stubNetworkClient = StubNetworkClient(emulateError: true) //говорим что хотим эмулировать ошибку
+        let loader = MoviesLoader(networkClient: stubNetworkClient)
         
         //When
+        let expectation = expectation(description: "Loading expectation")
         
-        //Then
-        
+        loader.loadMovies {result in
+            //Then
+            switch result {
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            case .success(_):
+                XCTFail("Unexpected failure")
+            }
+        }
+        waitForExpectations(timeout: 1)
     }
+    
 }
