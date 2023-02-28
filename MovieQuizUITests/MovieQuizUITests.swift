8c9e292ebdd9533f7c1dca1bf72ff4e09a5d155a
dch000 <dmitriy.ev.ch@yandex.ru>
2023-02-28 23:20:42 +0300

    Индивидуальное задание UI

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -27,6 +27,52 @@ final class MovieQuizUITests: XCTestCase {
         app.launch()
     }
     
-    func testScreenCast() throws {}
+    func testYesButton() {
+        sleep(3)
+        
+        let firstPoster = app.images["Poster"] //находим первоначальный постер
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["Yes"].tap() //находим кнопку ДА и нажимаем ее
+        sleep(3)
+        
+        let indexLabel = app.staticTexts["Index"]
+        let secondPoster = app.images["Poster"] //еще раз находим постер
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData) //проверяем что постеры разные
+        XCTAssertEqual(indexLabel.label, "2/10")
+    }
+    
+    func testNoButton() {
+        sleep(3)
+        
+        let firstPoster = app.images["Poster"] //находим первоначальный постер
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["No"].tap() //находим кнопку НЕТ и нажимаем ее
+        sleep(3)
+        
+        let indexLabel = app.staticTexts["Index"]
+        let secondPoster = app.images["Poster"] //еще раз находим постер
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData) //проверяем что постеры разные
+        XCTAssertEqual(indexLabel.label, "2/10")
+    }
+    
+    func testGameFinish() {
+        sleep(3)
+        for _ in 1...10{
+            app.buttons["No"].tap()
+            sleep(3)
+        }
+        
+        let alert = app.alerts["Game results"]
+        
+        XCTAssertTrue(alert.exists)
+        XCTAssertTrue(alert.label == "Этот раунд окончен!")
+        XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть еще раз")
+    }
 
 }
